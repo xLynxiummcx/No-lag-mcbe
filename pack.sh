710a51fc883aa734417b76e05a5d2642a504266f
xLynxiummcx <lynxiummc@outlook.com>
2024-06-21 03:15:18 +0200

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-BUILD_DIR=build/Android
-ZIP_FILE=build/Android.zip
+BUILD_SCRIPT="./Build.sh"
+BUILD_DIR="build"
+ANDROID_BUILD_DIR="$BUILD_DIR/Android"
+ZIP_FILE="$BUILD_DIR/Android.zip"
 
-echo "Creating zip archive of build files..."
+# Run the build script
+echo "Running build script..."
+$BUILD_SCRIPT -p Android -m your_material_file -t 4
 
 # Ensure the build directory exists
-if [ -d "$BUILD_DIR" ]; then
-  cd $BUILD_DIR
-  zip -r ../Android.zip ./*
-  cd -
+if [ -d "$ANDROID_BUILD_DIR" ]; then
+  echo "Creating zip archive of build files..."
+  zip -r $ZIP_FILE $ANDROID_BUILD_DIR/*
   echo "Zip archive created at $ZIP_FILE"
 else
-  echo "Error: Build directory $BUILD_DIR does not exist"
+  echo "Error: Build directory $ANDROID_BUILD_DIR does not exist"
   exit 1
 fi

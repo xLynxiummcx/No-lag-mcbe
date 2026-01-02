7dc5f4553475d3e44162ffdb6cb5bb9c6cac2af6
xLynxiummcx <lynxiummc@outlook.com>
2026-01-02 10:08:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,8 +11,7 @@ python -m pip install lazurite==0.6.0
 echo "[3/3] Downloading shaderc for Windows..."
 curl -L -o shaderc.exe "https://github.com/devendrn/newb-shader/releases/download/dev/shaderc-win-x64.exe"
 
-# Rename for easier usage in Bash
-mv shaderc.exe shaderc
-chmod +x shaderc
+# On Windows, keep the .exe extension
+chmod +x shaderc.exe
 
 echo "Setup complete!"
